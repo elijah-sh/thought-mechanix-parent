87e5fcd590be07364f4ec96110567cf8d3c385cd
沈帅虎 <shuaihu.shen@hand-china.com>
2020-12-14 09:41:34 +0800

    first commit

---
+++ config-server/src/main/java/com/thoughtmechanix/configurationserver/ConfigServeApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtmechanix.configurationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServeApplication.class, args);
+    }
+
+}
